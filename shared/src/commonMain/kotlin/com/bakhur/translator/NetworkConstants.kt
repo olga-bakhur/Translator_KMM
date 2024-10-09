8e5ca4f240ec3b2b151b9fb12624ae641679d07e
o.bakhur <o.bakhur@vironit.co.uk>
2024-10-09 11:49:26 +0200

    Add Ktor basic setup

---
+++ shared/src/commonMain/kotlin/com/bakhur/translator/NetworkConstants.kt
@@ -0,0 +1,5 @@
+package com.bakhur.translator
+
+object NetworkConstants {
+    const val BASE_URL = "https://translate.pl-coding.com"
+}
---
+++ shared/src/commonMain/kotlin/com/bakhur/translator/translate/data/remote/HttpClientFactory.kt
@@ -0,0 +1,7 @@
+package com.bakhur.translator.translate.data.remote
+
+import io.ktor.client.HttpClient
+
+expect class HttpClientFactory {
+    fun create(): HttpClient
+}
