8f35028e05dfe37cf1ebdc2ac6e3da9997bcfd74
o.bakhur <o.bakhur@vironit.co.uk>
2024-10-22 11:22:53 +0200

    Fix DI scope at provideVoiceToTextParser

--- androidApp/src/main/java/com/bakhur/translator/android/voice_to_text/di/VoiceToTextModule.kt
+++ androidApp/src/main/java/com/bakhur/translator/android/voice_to_text/di/VoiceToTextModule.kt
@@ -7,14 +7,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewScoped
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object VoiceToTextModule {
 
     @Provides
-    @ViewScoped
+    @ViewModelScoped
     fun provideVoiceToTextParser(context: Application): VoiceToTextParser =
         AndroidVoiceToTextParser(context)
 }
